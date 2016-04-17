479724a0640cc4f4503287f38d9eec9c2d81a11a
Sarah Vessels <cheshire137@github.com>
2016-04-17 18:39:34 -0500

    Add sqlite database and setup script

---
+++ setup.js
@@ -0,0 +1,12 @@
+var sqlite3 = require('sqlite3').verbose();
+var dbName = 'hue-steamer-' + process.env.NODE_ENV + '.sqlite';
+var db = new sqlite3.Database(dbName);
+
+console.log('Working in database ' + dbName);
+console.log('Creating bridge_connections...');
+db.serialize(function() {
+  db.run('CREATE TABLE IF NOT EXISTS bridge_connections ' +
+         '(id INTEGER, user TEXT, ip TEXT, PRIMARY KEY(id ASC))');
+});
+
+db.close();
